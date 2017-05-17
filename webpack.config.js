f5cbe2b9388d1c4a977605b803f8bebbb59f741c
Sean Carey <sean.carey@foxsports.com.au>
2017-05-17 18:29:56 +1000

    initial

--- webpack.config.js
+++ webpack.config.js
@@ -16,49 +16,49 @@ loaders.push({
 });
 
 module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    './src/index.jsx', // your app's entry point
-  ],
-  devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
-  output: {
-    publicPath: '/',
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    loaders
-  },
-  devServer: {
-    contentBase: "./public",
-    // do not print bundle build stats
-    noInfo: true,
-    // enable HMR
-    hot: true,
-    // embed the webpack-dev-server runtime into the bundle
-    inline: true,
-    // serve index.html in place of 404 responses to allow HTML5 history
-    historyApiFallback: true,
-    port: PORT,
-    host: HOST
-  },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin({
-      filename: 'style.css',
-      allChunks: true
-    }),
-    new DashboardPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-      files: {
-        css: ['style.css'],
-        js: [ "bundle.js"],
-      }
-    }),
-  ]
+    entry: [
+        'react-hot-loader/patch',
+        './src/index.js', // your app's entry point
+    ],
+    devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
+    output: {
+        publicPath: '/',
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders
+    },
+    devServer: {
+        contentBase: "./public",
+        // do not print bundle build stats
+        noInfo: true,
+        // enable HMR
+        hot: true,
+        // embed the webpack-dev-server runtime into the bundle
+        inline: true,
+        // serve index.html in place of 404 responses to allow HTML5 history
+        historyApiFallback: true,
+        port: PORT,
+        host: HOST
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin({
+            filename: 'style.css',
+            allChunks: true
+        }),
+        new DashboardPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            files: {
+                css: ['style.css'],
+                js: [ "bundle.js"],
+            }
+        }),
+    ]
 };
